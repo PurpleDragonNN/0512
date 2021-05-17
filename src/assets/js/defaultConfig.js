a1575f56907224c8d22c99ee9e1751821674a3e0
PurpleDragonNN <576817275@qq.com>
2021-05-18 00:38:32 +0800

    feat:折线图、数字

---
+++ src/assets/js/defaultConfig.js
@@ -0,0 +1,60 @@
+// chats默认配置
+const config = {
+  color: ['#fff','#fff','#fff'],
+  title: {
+    style: {
+      fontSize: 37,
+      fill: '#fff',
+    }
+  },
+  xAxis: {
+    axisLine: {
+      style: {
+        stroke: 'rgba(255,255,255, 0.2)'
+      }
+    },
+    axisTick: {
+      style: {
+        stroke: 'rgba(255,255,255, 0.2)'
+      }
+    },
+    axisLabel: {
+      style: {
+        fill: '#fff',
+        fontSize: 12,
+      }
+    },
+    nameTextStyle: {
+      fill: '#fff',
+      fontSize: 14,
+    }
+  },
+  yAxis: {
+    axisLine: {
+      style: {
+        stroke: 'rgba(255,255,255, 0.2)',
+      }
+    },
+    axisTick: {
+      style: {
+        stroke: 'rgba(255,255,255, 0.2)'
+      }
+    },
+    axisLabel: {
+      style: {
+        fill: '#fff',
+        fontSize: 12,
+      }
+    },
+    nameTextStyle: {
+      fill: '#fff',
+      fontSize: 14,
+    },
+    splitLine: {
+      style: {
+        stroke: 'rgba(255,255,255, 0.2)'
+      }
+    }
+  },
+}
+export default config
--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import dataV from '@jiaminghi/data-view'
-
+import Charts from '@jiaminghi/charts'
+import { changeDefaultConfig } from '@jiaminghi/charts'
+import chartsConfig from './assets/js/defaultConfig'
 Vue.use(dataV)
 const echarts = require('echarts');
 Vue.config.productionTip = false
 Vue.prototype.$echarts = echarts
+Vue.prototype.$charts = Charts
+
+
+for (let [key, value] of Object.entries(chartsConfig)){
+  changeDefaultConfig(key, value)
+}
+
+
 new Vue({
   router,
   render: h => h(App)
